c27beb0d7fa0ce46ba34686af01021f739519ab6
Saad Qureshi <muhammad.saadq14@gmail.com>
2022-11-30 22:25:33 -0500

    Added Upper Body Text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import MainBody from './components/MainBody'
-import UpperText from './components/UpperText'
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainBody from "./components/MainBody";
+import UpperText from "./components/UpperText";
 
 function App() {
   return (
     <div className="container">
-    <Header/>
-    <UpperText/>
+      <div className="card">
+        <Header />
+        <UpperText />
+      </div>
     </div>
   );
 }
--- src/components/UpperText.jsx
+++ src/components/UpperText.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
-
+import React from "react";
 
 function UpperText() {
   return (
-    <div>
-        <h2>Mohammad Qureshi</h2>
-        <h5>Frontend Developer</h5>
-        <h6>Website Under Construction</h6>
-        <button type='butoon'> <i class="bi bi-linkedin"></i>LinkedIn</button>
+    <div className="upperText">
+      <h2 className="upperText__name">Mohammad Qureshi</h2>
+      <h5 className="upperText__job">Frontend Developer</h5>
+      <a
+        className="upperText__web"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        target="_blank"
+      >
+        Mohammad.Qureshi.com
+      </a>
+      <a
+        className="upperText__button"
+        href="https://www.linkedin.com/in/mohammad-qureshi-780b88250/"
+        target="_blank"
+      >
+        <button type="button">
+          {" "}
+          <i class="bi bi-linkedin"></i> LinkedIn
+        </button>
+      </a>
+
+      <h3>About</h3>
+      <p>
+        I am a nurse turned frontend designer that likes to create and discover
+        new things.{" "}
+      </p>
     </div>
-  )
+  );
 }
 
-export default UpperText
+export default UpperText;
